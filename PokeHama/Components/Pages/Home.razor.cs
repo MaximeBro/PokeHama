07e4d6d2911315a5070fcfdc7b60b2ca87a1500f
Maxime Brochard <maximebrochardpro@gmail.com>
2024-02-21 16:58:47 +0100

    Adds home page slider

--- PokeHama/Components/Pages/Home.razor.cs
+++ PokeHama/Components/Pages/Home.razor.cs
@@ -5,8 +5,20 @@ namespace PokeHama.Components.Pages;
 public partial class Home
 {
 	private List<string> _pokemons = new();
+	private List<string> _filtered = new();
 	private bool _loading;
 	private int _amountToDisplay = 807;
+
+	public int AmoutToDisplay
+	{
+		get => _amountToDisplay;
+		set
+		{
+			_amountToDisplay = value;
+			_filtered = _pokemons.Take(value).ToList();
+			StateHasChanged();
+		}
+	}
 	
 	
 	protected override Task OnInitializedAsync()
@@ -18,6 +30,7 @@ public partial class Home
 			_pokemons.Add($"{Hardcoded.IconUrl}{i}.png");
 		}
 
+		_filtered = _pokemons.ToList();
 		_loading = false;
 		return Task.CompletedTask;
 	}

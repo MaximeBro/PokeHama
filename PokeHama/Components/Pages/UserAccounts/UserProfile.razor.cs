ec448dff2f3fb0f7f1ca30736cc3819c7d8208d9
MaximeBro <maximebrochard.pro@gmail.com>
2024-03-05 00:50:30 +0100

    Adds personal information & password editing

--- PokeHama/Components/Pages/UserAccounts/UserProfile.razor.cs
+++ PokeHama/Components/Pages/UserAccounts/UserProfile.razor.cs
@@ -8,6 +8,7 @@ using PokeHama.Databases;
 using PokeHama.Extensions;
 using PokeHama.Models.Account;
 using PokeHama.Models.Account.Enums;
+using BC = BCrypt.Net.BCrypt;
 
 namespace PokeHama.Components.Pages.UserAccounts;
 
@@ -16,6 +17,7 @@ public partial class UserProfile
     [Inject] public IDbContextFactory<UtilityContext> UtilityFactory { get; set; } = null!;
     [Inject] public NavigationManager NavManager { get; set; } = null!;
     [Inject] public IDialogService DialogService { get; set; } = null!;
+    [Inject] public ISnackbar Snackbar { get; set; } = null!;
     [CascadingParameter] public Task<AuthenticationState> AuthenticationStateTask { get; set; } = null!;
     public int SelectedTab { get; set; } = 0;
 
@@ -66,11 +68,46 @@ public partial class UserProfile
     private async Task EditRelativeNameAsync()
     {
         var utilityDb = await UtilityFactory.CreateDbContextAsync();
+        var parameters = new DialogParameters<EditRelativeName> { { x => x.Firstname, _user!.FirstName }, { x => x.Lastname, _user!.LastName } };
+        var dialog = await DialogService.ShowAsync<EditRelativeName>(string.Empty, parameters, _dialogOptions);
+        var result = await dialog.Result;
+        if (result is { Data: Dictionary<string, string> data })
+        {
+            var oldUser = utilityDb.Users.AsTracking().FirstOrDefault(x => x.Username == _user!.Username);
+            if (oldUser != null)
+            {
+                oldUser.FirstName = data["firstname"];
+                oldUser.LastName = data["lastname"];
+                await utilityDb.SaveChangesAsync();
+            }
+            await utilityDb.DisposeAsync();
+            await RefreshDataAsync();
+            Snackbar.Configuration.PositionClass = Defaults.Classes.Position.BottomCenter;
+            Snackbar.Add("Vos données personnelles ont bien été mises à jour.", Severity.Success, options =>
+            {
+                options.VisibleStateDuration = 2000;
+                options.ShowCloseIcon = false;
+            });
+        }
     }
 
     private async Task EditPasswordAsync()
     {
         var utilityDb = await UtilityFactory.CreateDbContextAsync();
+        var parameters = new DialogParameters<EditPassword> { { x => x.OldPasswordHash, _user!.Password } };
+        var dialog = await DialogService.ShowAsync<EditPassword>(string.Empty, parameters, _dialogOptions);
+        var result = await dialog.Result;
+        if (result is { Data: string password })
+        {
+            var oldUser = utilityDb.Users.AsTracking().FirstOrDefault(x => x.Username == _user!.Username);
+            if (oldUser != null)
+            {
+                oldUser.Password = BC.HashPassword(password);
+                await utilityDb.SaveChangesAsync();
+            }
+            await utilityDb.DisposeAsync();
+            NavManager.NavigateTo("api/authenticate/logout", true);
+        }
     }
     
     private async Task EditAccountPrivacyAsync(AccountPrivacy value)
